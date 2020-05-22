f10449fe494c6c081f0e6152b66735c9a77e1e34
Denis Putnam <denisputnam@verizon.net>
2020-05-22 09:06:31 -0400

    Ran typedoc.

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,6 +1,10 @@
 import { HttpServer } from "./httpserver";
 import { Config } from "./config";
 
+/**
+ * @classdesc - This is the main of the wallet server
+ * @class Wallet
+ */
 export class Wallet {
     public httpServer: HttpServer;
     public config: Config = new Config()
