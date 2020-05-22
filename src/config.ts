4bbee5b1fe650a8d55fd420483284e72938779d0
Denis Putnam <denisputnam@verizon.net>
2020-05-22 09:23:12 -0400

    Changed default port to 4001.

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ export class Config {
      */
     constructor() {
         this.defaultServerHost = 'localhost';
-        this.defaultServerPort = 3001;
+        this.defaultServerPort = 4001;
         this.faucetPrivateKey = "838ff8634c41ba62467cc874ca156830ba55efe3e41ceeeeae5f3e77238f4eef";
         this.faucetPublicKey = '8c4431db61e9095d5794ff53a3ae4171c766cadef015f2e11bec22b98a80f74a0';
         this.faucetAddress = 'f3a1e69b6176052fcc4a3248f1c5a91dea308ca9';
